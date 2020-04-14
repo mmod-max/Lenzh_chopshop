af9b39428e758601b8ad5548c6b055b7eb8849a4
mmod-max <63668667+mmod-max@users.noreply.github.com>
2020-04-14 21:29:17 +0300

    Update for new weight system in es_extended (esx)
    Changed column 'limit' for 'weight'. Weights are in favor of players and it can be changed later

--- Lenzh_chopshop.sql
+++ Lenzh_chopshop.sql
@@ -1,12 +1,12 @@
 USE `essentialmode`;
 
 
-INSERT INTO `items` (`name`, `label`, `limit`) VALUES
-  ('battery', 'Car Battery', 2),
-  ('lowradio', 'Stock Radio', 5),
-  ('stockrim', 'Stock Rim', 4),
-  ('airbag', 'Airbag', 7),
-  ('highradio', 'Aftermarket Radio', 5),
-  ('highrim', 'Nice Rim', 4)
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES
+  ('battery', 'Car Battery', 4),
+  ('lowradio', 'Stock Radio', 2),
+  ('stockrim', 'Stock Rim', 5),
+  ('airbag', 'Airbag', 2,
+  ('highradio', 'Aftermarket Radio', 2),
+  ('highrim', 'Nice Rim', 6)
 
 ;
